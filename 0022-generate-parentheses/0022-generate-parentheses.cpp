5fc8a10dcd73627b4c31dc1355668b3ce0c277bc
ayushi <ayushi.singh0618@gmail.com>
2026-08-21 12:04:58 +0530

    Time: 0 ms (100%), Space: 13 MB (83.15%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void backtrack(int open, int close, int n, string& current, vector<string>& result) {
+        if (current.length() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+
+        if (open < n) {
+            current.push_back('(');
+            backtrack(open + 1, close, n, current, result);
+            current.pop_back(); // backtrack
+        }
+
+        if (close < open) {
+            current.push_back(')');
+            backtrack(open, close + 1, n, current, result);
+            current.pop_back(); // backtrack
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current = "";
+        backtrack(0, 0, n, current, result);
+        return result;
+    }
+};
