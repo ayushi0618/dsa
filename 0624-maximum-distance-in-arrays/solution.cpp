e505d3176b72a03c5ec25675640c0eb4ee882cc3
ayushi <205179088+ayushi0618@users.noreply.github.com>
2026-09-07 12:00:00 +0530

    Solve LeetCode #624 - Maximum Distance in Arrays

---
+++ 0624-maximum-distance-in-arrays/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int minVal = arrays[0][0];
+        int maxVal = arrays[0].back();
+        int ans = 0;
+
+        for (int i = 1; i < arrays.size(); i++) {
+            ans = max(ans, max(abs(arrays[i].back() - minVal),
+                              abs(maxVal - arrays[i][0])));
+
+            minVal = min(minVal, arrays[i][0]);
+            maxVal = max(maxVal, arrays[i].back());
+        }
+
+        return ans;
+    }
+};
