2880ba85534b05037228d5805e3f924d984d513a
ayushi <205179088+ayushi0618@users.noreply.github.com>
2026-09-08 12:00:00 +0530

    Solve LeetCode #579 - Find Cumulative Salary of an Employee

---
+++ 0579-find-cumulative-salary-of-an-employee/solution.sql
@@ -0,0 +1,15 @@
+SELECT
+    id,
+    month,
+    SUM(salary) OVER (
+        PARTITION BY id
+        ORDER BY month
+        RANGE 2 PRECEDING
+    ) AS Salary
+FROM Employee
+WHERE (id, month) NOT IN (
+    SELECT id, MAX(month)
+    FROM Employee
+    GROUP BY id
+)
+ORDER BY id, month DESC;
