4ba6dcac5d74fc66e5dd82d11aa30e09658d71b6
ayushi <ayushi.singh0618@gmail.com>
2026-09-26 15:21:59 +0530

    Time: 0 ms (100%), Space: 9 MB (51.76%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,36 +1,22 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    // One-argument version: total commas from 1..n
     long long countCommas(long long n) {
-        if (n < 1000) return 0;
-        long long res = 0;
-        // thresholds: 10^3, 10^6, 10^9, 10^12, 10^15, 10^18
-        const vector<long long> th = {
-            1000LL,
-            1000000LL,
-            1000000000LL,
-            1000000000000LL,
-            1000000000000000LL,
-            1000000000000000000LL
-        };
-        for (int i = 0; i < (int)th.size(); ++i) {
-            long long start = th[i];
-            if (n < start) break;
-            long long end = (i + 1 < (int)th.size() ? th[i+1] - 1 : n);
-            long long upper = min(n, end);
-            res += (upper - start + 1) * (long long)(i + 1);
+        long long totalCommas = 0;
+        
+        // Boundaries for numbers having 1, 2, 3, 4, 5 commas respectively
+        // 10^3 = 1000, 10^6 = 1000000, 10^9 = 1000000000, etc.
+        long long p = 1000;
+        long long commas = 1;
+        
+        while (p <= n) {
+            long long next_p = p * 1000;
+            long long count = min(n, next_p - 1) - p + 1;
+            totalCommas += count * commas;
+            
+            p = next_p;
+            commas++;
         }
-        return res;
-    }
-
-    // Two-argument version: commas in [L, R]
-    long long countCommas(int L, int R) {
-        long long l = (long long)L;
-        long long r = (long long)R;
-        return countCommas(r) - countCommas(l - 1);
+        
+        return totalCommas;
     }
 };
